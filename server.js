6f6b8b64fbae32b982bf1d5d5f8b9ed484fae549
CJRMSolutions <r-dev-solutions@outlook.com>
2025-04-14 21:14:36 -0600

    Updated CORS configuration to allow all origins

--- server.js
+++ server.js
@@ -40,6 +40,11 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model('User', UserSchema);
 
 // Middleware
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
